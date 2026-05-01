05f49c0dab8fb9809ce7170cd9a14a1a85022c14
AlenaSubbota <alenasubbota@bk.ru>
2026-05-02 00:07:55 +0800

    chore: drop unused Link import in BoostyClaimBlock

--- src/components/reader/BoostyClaimBlock.tsx
+++ src/components/reader/BoostyClaimBlock.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import Link from 'next/link';
 import { createClient } from '@/utils/supabase/client';
 import { useToasts, ToastStack } from '@/components/ui/Toast';
 
